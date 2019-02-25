fd45ef4491f205d2e2f96857d66e90fead41f8a8
Logan Zartman <logan.zartman@utexas.edu>
2019-02-25 02:06:46 -0600

    basic port from pytermfx to termpixels

--- termworm.py
+++ termworm.py
@@ -1,10 +1,7 @@
-from pytermfx import Terminal, Color, Style
-from pytermfx.tools import TerminalApp, draw_hline, print_hcenter
+from termpixels.app import App
+from termpixels.screen import Color
 from random import randint, choice
 import time
-
-t = Terminal()
-t.cursor_set_visible(False)
 
 K_UP = "up"
 K_DOWN = "down"
@@ -13,44 +10,123 @@ K_LEFT = "left"
 
 game_x = 0
 game_y = 3
-game_w = 0
-game_h = 0
+game_w = 4
+game_h = 4
 
-def resize():
-    global t, game_x, game_y, game_w, game_h
-    game_w = int(t.w / 2 - game_x)
-    game_h = t.h - game_y
-t.add_resize_handler(resize)
+class GameOver(BaseException):
+    def __init__(self, message=""):
+        super().__init__(message)
 
-class Snake:
+class TermwormApp(App):
+    def __init__(self):
+        super().__init__()
+        self.mode = "game"
+        self.worm = Worm(int(game_w/2), int(game_h/2))
+        
+        self.score = 0
+        self.hiscore = 0
+        self.t0 = time.perf_counter()
+
+        self.on_resize()
+        self.food_x = self.worm.x
+        self.food_y = self.worm.y
+
+    def on_resize(self):
+        global game_w, game_h
+        game_w = int(self.screen.w / 2 - game_x)
+        game_h = self.screen.h - game_y
+        self.move_food()
+
+    def on_frame(self):
+        if self.mode == "game":
+            delay = max(0.1 - self.score / 50 * 0.1, 0.)
+            if time.perf_counter() - self.t0 >= delay:
+                self.t0 = time.perf_counter()
+                try:
+                    self.update_game()
+                except GameOver:
+                    self.mode = "game-over"
+        if self.mode == "game-over":
+            self.update_endgame()
+
+    def on_key(self, c):
+        if c == K_LEFT:
+            if not self.worm.intersecting(self.worm.x-1, self.worm.y):
+                self.worm.vx = -1
+                self.worm.vy = 0
+        elif c == K_RIGHT:
+            if not self.worm.intersecting(self.worm.x+1, self.worm.y):
+                self.worm.vx = 1
+                self.worm.vy = 0
+        elif c == K_UP:
+            if not self.worm.intersecting(self.worm.x, self.worm.y-1):
+                self.worm.vx = 0
+                self.worm.vy = -1
+        elif c == K_DOWN:
+            if not self.worm.intersecting(self.worm.x, self.worm.y+1):
+                self.worm.vx = 0
+                self.worm.vy = 1
+        
+    def update_game(self):
+        # app.framerate = int(min(20, score / 5) + 10)
+        self.screen.clear()
+        self.worm.update(self.screen)
+        if self.worm.x == self.food_x and self.worm.y == self.food_y:
+            self.move_food()
+            self.worm.length += 1
+            self.score += 1
+            self.hiscore = max(self.hiscore, self.score)
+        self.draw_ui()
+        self.draw_food()
+        self.screen.update()
+
+    def update_endgame(self):
+        # Draw endgame screen
+        self.screen.clear()
+        w = self.screen.w
+        h = self.screen.h
+        self.screen.print("Game over!", w // 2 - 5, h // 3, fg=Color.rgb(1,1,0))
+        self.screen.print("You scored: {}".format(self.score), w // 2 - 7, h // 3 + 1, fg=Color.rgb(1,1,1))
+        self.screen.print("Press CTRL+C to quit.", w // 2 - 10, h // 3 + 2, fg=Color.rgb(0.5,0.5,0.5))
+        self.screen.update()
+
+    def draw_ui(self):
+        self.screen.print("~TermWorm~", 0, 0, fg=Color.rgb(1,1,1))
+        self.screen.print("░ Score: {}".format(self.score), 0, 1, fg=Color.rgb(0.5,0.5,0.5))
+        self.screen.print("─" * self.screen.w, 0, 2, fg=Color.rgb(0.5,0.5,0.5))
+
+    def draw_food(self):
+        self.screen.print("░░", self.food_x * 2 + game_x, self.food_y + game_y)
+
+    def move_food(self):
+        positions = [(x,y) for x in range(game_w) for y in range(game_h)
+                     if not self.worm.intersecting(x, y)]
+        self.food_x, self.food_y = choice(positions)
+
+class Worm:
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.vx = 1
         self.vy = 0
-        self.length = 36
+        self.length = 5
         self.old = []
 
-    def update(self):
-        global app
+    def update(self, screen):
         self.old += [(self.x, self.y)]
         if len(self.old) > self.length:
-            old = self.old.pop(0)
-            t.cursor_to(old[0] * 2 + game_x, old[1] + game_y)
-            t.write("  ")
+            self.old.pop(0)
+
+        for x, y in self.old:
+            col = Color.hsl(time.monotonic() * 0.1, 1.0, 0.7)
+            screen.print("██", x * 2 + game_x, y + game_y, fg=col)
 
         if self.intersecting(self.x + self.vx, self.y + self.vy):
-            app.stop(game_end)
-            return
+            raise GameOver()
 
         self.x += self.vx
         self.y += self.vy
         self.wrap()
-
-        t.cursor_to(snake.x * 2 + game_x, snake.y + game_y)
-        t.style(Color.hsl(time.monotonic() * 0.1, 1.0, 0.7))
-        t.write("██")
-        t.flush()
 
     def intersecting(self, x, y):
         return any(x == pos[0] and y == pos[1] for pos in self.old[1:])
@@ -65,91 +141,4 @@ class Snake:
         if self.y >= game_h:
             self.y = 0
 
-snake = Snake(int(game_w/2), int(game_h/2))
-input_q = []
-score = 24
-hiscore = 0
-food_x = snake.x
-food_y = snake.y
-
-def update():
-    global food_x, food_y, score, hiscore, app
-    app.framerate = int(min(20, score / 5) + 10)
-    process_input()
-    snake.update()
-    if snake.x == food_x and snake.y == food_y:
-        move_food()
-        snake.length += 1
-        score += 1
-        hiscore = max(hiscore, score)
-        draw_ui()
-
-def draw_ui():
-    global t, score, hiscore
-    t.cursor_to(0, 0)
-    t.style(Color.hex(0xFFFFFF)).writeln("~TermWorm~")
-    t.style(Color.hex(0x777777)).writeln("░ Score: ", score).flush()
-    t.style(Color.hex(0x777777))
-    draw_hline(t, 2)
-t.add_resize_handler(draw_ui)
-
-def move_food():
-    global t, food_x, food_y, snake
-    positions = [(x,y) for x in range(game_w) for y in range(game_h)
-        if not snake.intersecting(x, y)]
-    food_x, food_y = choice(positions)
-    t.cursor_to(food_x * 2 + game_x, food_y + game_y)
-    t.write("░░")
-    t.flush()
-t.add_resize_handler(move_food)
-
-def process_input():
-    global input_q
-    for i, c in enumerate(input_q):
-        if c == K_LEFT:
-            if not snake.intersecting(snake.x-1, snake.y):
-                snake.vx = -1
-                snake.vy = 0
-                input_q = input_q[i+1:]
-        elif c == K_RIGHT:
-            if not snake.intersecting(snake.x+1, snake.y):
-                snake.vx = 1
-                snake.vy = 0
-                input_q = input_q[i+1:]
-        elif c == K_UP:
-            if not snake.intersecting(snake.x, snake.y-1):
-                snake.vx = 0
-                snake.vy = -1
-                input_q = input_q[i+1:]
-        elif c == K_DOWN:
-            if not snake.intersecting(snake.x, snake.y+1):
-                snake.vx = 0
-                snake.vy = 1
-                input_q = input_q[i+1:]
-
-def on_input(c):
-    global input_q
-    if c == "q":
-        app.stop(game_end)
-        return
-    input_q.append(c)
-
-def game_end():
-    global t
-
-    # Draw endgame screen
-    t.clear()
-    t.style(Color.hex(0xFFFF00))
-    print_hcenter(t, "Game over!", t.h // 3)
-    t.style(Color.hex(0xFFFFFF))
-    print_hcenter(t, "You scored: {}".format(score), t.h // 3 + 1)
-    t.style(Color.hex(0x777777))
-    print_hcenter(t, "Press q to quit.", t.h // 3 + 2)
-    t.flush()
-    
-    # Wait for keypress
-    while t.getch() != "q":
-        pass
-
-app = TerminalApp(t, 10, update = update, on_input = on_input)
-app.start()
+TermwormApp().start()
